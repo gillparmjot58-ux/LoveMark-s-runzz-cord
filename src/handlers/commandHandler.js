c6ad1bf0df5a2e7352fa7db53911f76c85fbe773
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-18 11:36:15 +0530

    Update commandHandler.js

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,1 +1,16 @@
+const fs = require("fs");
+const path = require("path");
 
+module.exports = (client) => {
+    const commandsPath = path.join(__dirname, "../commands");
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+    client.commands = new Map();
+
+    for (const file of commandFiles) {
+        const command = require(`${commandsPath}/${file}`);
+        client.commands.set(command.data.name, command);
+    }
+
+    console.log("Commands Loaded");
+};
